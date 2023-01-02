d7cb4c01109cef970ff24d314b94cf96e52a4f1a
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-03 00:51:38 +0900

    Javaプロワークシート第4回まで完了

--- src/Array1DMap.java
+++ src/Array1DMap.java
@@ -8,9 +8,39 @@ public class Array1DMap {
 
         int x = 2;
 
-        while (){
-            for (int i=0; i<=array.length; i++){
+        while (true){
+            for (int i=0; i<array.length; i++){
+                if (i == x){
+                    array[i] = 1;
+                }
+                else {
+                    array[i] = 0;
+                }
+                System.out.println(array[i] + "");
+            }
+            System.out.println("どちらに移動する？");
 
+            String key = scanner.nextLine();
+            switch (key){
+                case "a":
+                    if(x <= 0){
+                        System.out.println("左に移動できません。再入力してください");
+                        break;
+                    }
+                    x = x - 1;
+                    System.out.println("左に移動");
+                    break;
+                case "d":
+                    if(x >= 4){
+                        System.out.println("右に移動できません。再入力してください");
+                        break;
+                    }
+                    x = x + 1;
+                    System.out.println("右に移動");
+                    break;
+                case "q":
+                    System.out.println("終了");
+                    System.exit(0);
             }
         }
     }
---
+++ src/Array2DMap.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Array2DMap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] array = new int[5][5];
+
+        int locationX = 0;
+        int locationY = 0;
+        array[locationY][locationX] = 1;
+
+        while (true){
+            for (int y=0; y<array.length; y++){
+                for (int x=0; x<array[y].length; x++){
+                    if (locationX == x  && locationY == y){
+                        array[y][x] = 1;
+                    }
+                    else {
+                        array[y][x] = 0;
+                    }
+                    System.out.print(array[y][x] + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("どちらに移動する？");
+
+            String key = scanner.next();
+
+            switch (key){
+                case "a":
+                    if(locationX <= 0){
+                        System.out.println("左に移動できません。再入力してください");
+                        break;
+                    }
+                    locationX = locationX - 1;
+                    System.out.println("左に移動");
+                    break;
+                case "d":
+                    if(locationX >= 4){
+                        System.out.println("右に移動できません。再入力してください");
+                        break;
+                    }
+                    locationX = locationX + 1;
+                    System.out.println("右に移動");
+                    break;
+                case "w":
+                    if(locationY <= 0){
+                        System.out.println("上に移動できません。再入力してください");
+                        break;
+                    }
+                    locationY = locationY - 1;
+                    System.out.println("上に移動");
+                    break;
+                case "s":
+                    if(locationY >= 4){
+                        System.out.println("下に移動できません。再入力してください");
+                        break;
+                    }
+                    locationY = locationY + 1;
+                    System.out.println("下に移動");
+                    break;
+                case "q":
+                    System.out.println("終了");
+                    System.exit(0);
+            }
+
+        }
+
+    }
+}

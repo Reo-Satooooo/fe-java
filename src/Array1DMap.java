89f29d276522bd300e766d21f07ae926b5732786
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-02 23:29:43 +0900

    first commit

---
+++ src/Array1DMap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Array1DMap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[5];
+
+        int x = 2;
+
+        while (){
+            for (int i=0; i<=array.length; i++){
+
+            }
+        }
+    }
+}

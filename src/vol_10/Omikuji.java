22d63a5e577af71e62e97d14ebcff96d09514225
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-10 23:22:35 +0900

    第10回課題１

---
+++ src/vol_10/Omikuji.java
@@ -0,0 +1,18 @@
+package vol_10;
+
+public class Omikuji {
+
+    private String unsei;
+    private String setumei;
+
+    public Omikuji(String unsei, String setumei){
+        this.unsei = unsei;
+        this.setumei = setumei;
+    }
+
+    public void print(){
+        String str = "今日の運勢：" + unsei + "、" + setumei;
+        System.out.println(str);
+    }
+
+}
---
+++ src/vol_10/OmikujiBox.java
@@ -0,0 +1,14 @@
+package vol_10;
+
+import java.util.Random;
+
+public class OmikujiBox {
+
+    private Omikuji[] omikujis;
+
+    public OmikujiBox(){
+        this.omikujis = new Omikuji[5];
+        omikujis[0] = new Omikuji("大吉","今日は絶好調");
+    }
+
+}

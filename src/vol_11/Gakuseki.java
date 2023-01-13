3f7305a8fc356b678d7b95cd1dd016ee7270a89c
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-13 23:57:10 +0900

    第11回課題１

---
+++ src/vol_11/Gakuseki.java
@@ -0,0 +1,19 @@
+package vol_11;
+
+public class Gakuseki {
+
+    private String number;
+
+    public Gakuseki(String number){
+        if(number == null){
+            throw new RuntimeException();
+        }
+        this.number = number;
+    }
+
+    public void print(){
+        String msg = "学籍番号："+this.number;
+        System.out.println(msg);
+    }
+
+}
---
+++ src/vol_11/KadaiA1.java
@@ -0,0 +1,13 @@
+package vol_11;
+
+public class KadaiA1 {
+
+    public static void main(String[] args) {
+        Gakuseki g1 = new Gakuseki("b2191040");
+        g1.print();
+        Gakuseki g2 = new Gakuseki(null);
+        g2.print();
+        System.out.println("終了");
+    }
+
+}
